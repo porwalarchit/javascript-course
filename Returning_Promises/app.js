686494628cc2bf582d9f5ef746e2d532bc56dff6
porwalarchit <architporwal@gmail.com>
2023-05-09 15:01:40 +0530

    Example that illustrates how to return promises from function

--- Returning_Promises/app.js
+++ Returning_Promises/app.js
@@ -1,15 +1,39 @@
-const willGetYouADog = new Promise((resolve, reject)=>{
-    const rand = Math.random();
-    console.log(rand);
-    if(rand < 0.5){
-        resolve()
-    }else{
-        reject();
-    }
-});
+// const willGetYouADog = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         const rand = Math.random();
+//         console.log(rand);
+//         if(rand < 0.5){
+//             resolve()
+//         }else{
+//             reject();
+//         }
+//     }, 3000);
+// });
+// willGetYouADog.then(()=>{
+//     console.log("Yay, you got a Dog :)");
+// });
+// willGetYouADog.catch(()=>{
+//     console.log("Oops, you didn't got a dog!!");
+// });
 
-willGetYouADog.then(()=>{
-    console.log("Yay, you got a Dog");
-}).catch(()=>{
-    console.log("Oops, you didn't got a dog!!");
+const makeDogPromise = () =>{
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            const rand = Math.random();
+            console.log(rand);
+            if(rand < 0.5){
+                resolve()
+            }else{
+                reject();
+            }
+        }, 2000);
+    });
+};
+
+makeDogPromise()
+.then(()=>{
+    console.log("Yay, you got a Dog :)");
 })
+.catch(()=>{
+    console.log("Oops, you didn't got a dog!!");
+});
