254374772997be56bf7b97dfb1a8e4495c5c7bc1
porwalarchit <architporwal@gmail.com>
2023-05-09 11:06:51 +0530

    Used change and also created a single loop to get data in the formData object.

--- Input_Change_Events/app.js
+++ Input_Change_Events/app.js
@@ -3,19 +3,27 @@ const termsCheckbox = document.querySelector('#terms');
 const veggieSelect = document.querySelector('#veggie');
 
 const formData = {};
+for(let input of [creditCardInput, termsCheckbox, veggieSelect]){
+	input.addEventListener('change', ({target})=>{
+		const {name, type, value, checked} = target;
+		formData[name] = type === 'checkbox' ? checked : value;
+		// formData[e.target.name] = e.target.value;
+	})
+}
 
-creditCardInput.addEventListener('input', (e)=>{
-	console.log('Credit Card data changed', e);
-	formData['cc'] = e.target.value;
-})
 
-termsCheckbox.addEventListener('input', (e)=>{
-	console.log('Checkbox data changed', e);
-	formData['agreeToTerms'] = e.target.checked;
-})
+// creditCardInput.addEventListener('input', (e)=>{
+// 	console.log('Credit Card data changed', e);
+// 	formData['cc'] = e.target.value;
+// })
 
-veggieSelect.addEventListener('input', (e)=>{
-	console.log('Veggie data changed', e);
-	formData['veggie'] = e.target.value;
-})
+// termsCheckbox.addEventListener('input', (e)=>{
+// 	console.log('Checkbox data changed', e);
+// 	formData['agreeToTerms'] = e.target.checked;
+// })
+
+// veggieSelect.addEventListener('input', (e)=>{
+// 	console.log('Veggie data changed', e);
+// 	formData['veggie'] = e.target.value;
+// })
 
