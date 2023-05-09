bcfd5612de7ccca3f26419ae289c5bd277359a81
porwalarchit <architporwal@gmail.com>
2023-05-09 17:13:40 +0530

    Chaining XMLHttpRequest.

--- XMLHttpRequests/app.js
+++ XMLHttpRequests/app.js
@@ -1,11 +1,23 @@
 const firstReq = new XMLHttpRequest();
 
-firstReq.addEventListener('load', ()=>{
-	console.log("It worked!!!");
-	const data = JSON.parse(firstReq.responseText);
-	for(let planet of data.results){
-		console.log(planet.name); 
-	}
+firstReq.addEventListener('load', function(){
+	console.log("First Request Worked!!!");
+	const data = JSON.parse(this.responseText);
+	// for(let planet of data.results){
+	// 	console.log(planet.name); 
+	// }
+	const filmUrl = data.results[0].films[0];
+	const filmReq = new XMLHttpRequest();
+	filmReq.addEventListener('load', function(){
+		console.log("Second Request Worked!!!");
+		const filmData = JSON.parse(this.responseText);
+		console.log(filmData);
+	})
+	filmReq.addEventListener('error', function(e){
+		console.log('ERROR!!', e);
+	})
+	filmReq.open('GET', filmUrl);
+	filmReq.send();
 });
 firstReq.addEventListener('error', ()=>{
 	console.log('Error');
@@ -13,4 +25,4 @@ firstReq.addEventListener('error', ()=>{
 
 firstReq.open('GET', 'https://swapi.dev/api/planets');
 firstReq.send();
-console.log("Request Sent!!");
+console.log("First Request Sent!!");
