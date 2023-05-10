7d802f225c7fbb2b151c9618dee19c61b9bcdf36
porwalarchit <architporwal@gmail.com>
2023-05-10 10:40:12 +0530

    Classes in JS

--- Classes/app.js
+++ Classes/app.js
@@ -8,10 +8,24 @@ class Color {
 	greet() {
 		console.log(`Hello from ${this.name}!!`);
 	}
-	rgb() {
+	// rgb() {
+	// 	const { r, g, b } = this;
+	// 	return `rgb(${r}, ${g}, ${b})`;
+	// }
+
+	innerRGB(){
 		const { r, g, b } = this;
-		return `rgb(${r}, ${g}, ${b})`;
+		return `${r}, ${g}, ${b}`;
 	}
+
+	rgb(){
+		return `rgb(${this.innerRGB()})`;
+	}
+
+	rgba(a=1.0){
+		return `rgba(${this.innerRGB()}, ${a})`;
+	}
+
 	hex() {
 		const { r, g, b } = this;
 		return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
@@ -21,4 +35,5 @@ class Color {
 const c1 = new Color(255, 67, 89, 'tomato');
 c1.greet();
 console.log("RGB Color: ", c1.rgb());
+console.log("RGBA Color: ", c1.rgba());
 console.log("Hex Color: ", c1.hex());
